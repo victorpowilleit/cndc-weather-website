77699ca303715d5ac4d98a9d1e3c1aef708a8ec9
Victor Powilleit <victor.powilleit@gmail.com>
2020-04-14 03:53:13 -0300

    fix loader on 2nd message field

--- public/js/app.js
+++ public/js/app.js
@@ -3,10 +3,10 @@ const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const msg1 = document.querySelector('#msg1')
 const msg2 = document.querySelector('#msg2')
-msg1.textContent = '';
-msg2.textContent = '';
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
+  msg1.textContent = '';
+  msg2.textContent = '';
   const location = search.value;
   msg1.textContent = 'Loading...';
   fetch("/weather?address=" + location).then((res) => {
